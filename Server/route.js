e6be95319dbb5d37f8c0dc6811771fa070bee3b3
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-08-30 19:45:45 +0530

    30-08-2024

---
+++ Server/route.js
@@ -0,0 +1,60 @@
+// Implement Basic routing
+
+/*
+Some Basic Staus Code Ranges
+
+1. 100 Range - Informational Responses
+2. 200 Range - Success Responses
+3. 300 Range - Redirection Messages
+4. 400 Range - User or Client Error Responses
+5. 500 Range - Server Error Responses
+*/
+
+const http = require("http");
+const fs = require("fs");
+
+// Creating the server
+const server = http.createServer((req, res) => {
+  // Corrected the writeHead call
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  let path = "./Files/";
+  let url = req.url;
+  // Check which url path is being requested
+  switch (url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+
+      // Redirecting this path to about
+    case "/about-me":
+      res.statusCode = 301; // Permanent Redirect code
+      res.setHeader('Location', '/about');
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  // Send the corresponding html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+// Listening on the server
+server.listen(3000, "localhost", () => {
+  console.log("Server is running on port 3000");
+});
--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 
 // Importing the http module
 const http = require('http');
+const fs = require('fs');
 
 // Create a server
 
@@ -9,7 +10,31 @@ The 'req' object contains information about the request made by the user
 The 'res' object is used to send a response back to the user
 */
 const server = http.createServer((req, res) =>{
-    console.log('Request Made');
+
+    // Set header content type
+    // res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
+    // Send response back to the user
+    /*
+    res.write('<h1>Hello World</h1>');
+    res.write('<p>Hello Amigos</p>');
+    */
+
+    // Send the data from the html file in the Files folder
+    // Read the data from the file and send it
+    fs.readFile('./Files/index.html', (err, data) =>{ // Read the file
+        if(err){ // Check if there is any error
+            console.log(err);
+            res.end(); // End the respond so that the website does not stay hanging
+        }else{// Send the data since no error is present
+            // res.write(data);
+            /* If there is only one line before 'res.end()' we can also write like this */
+            res.end(data);
+        }
+    })
+
+    // End the response
+    // res.end();
 });
 
 server.listen(3000, 'localhost', () =>{
